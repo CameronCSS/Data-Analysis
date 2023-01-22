70d7585bf4c784f5cd6782b3913c73bea212ca33
Cameron Seamons <121735588+CameronCSS@users.noreply.github.com>
2023-01-21 22:40:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
     height: 1000,
-    width: 1200,
+    width: 1800,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
